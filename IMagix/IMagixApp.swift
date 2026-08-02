a2a7f58973f2fddeeedb1625b3268fdf7a6a9dca
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:06:28 -0400

    Add IMagix/IMagixApp.swift

---
+++ IMagix/IMagixApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMagixApp.swift
+//  IMagix
+//
+//  Created by Elliot Williams on 2025-07-04.
+//
+
+import SwiftUI
+
+@main
+struct IMagixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
