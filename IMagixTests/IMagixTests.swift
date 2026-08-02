950019014847585fe6fab0021493e754c9ae5dbb
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:06:35 -0400

    Add IMagixTests/IMagixTests.swift

---
+++ IMagixTests/IMagixTests.swift
@@ -0,0 +1,17 @@
+//
+//  IMagixTests.swift
+//  IMagixTests
+//
+//  Created by Elliot Williams on 2025-07-04.
+//
+
+import Testing
+@testable import IMagix
+
+struct IMagixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
